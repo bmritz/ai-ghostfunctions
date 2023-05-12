0b00d7d03cff815cbc7bf36bfb06a32860a52ed9
Brian Ritz <brianmritz@gmail.com>
2023-05-12 16:50:52 -0400

    fixes #68: simplifies code and allows calling non-keyword args with any formulation of the decorator

--- src/ai_ghostfunctions/ghostfunctions.py
+++ src/ai_ghostfunctions/ghostfunctions.py
@@ -172,6 +172,26 @@ def ghostfunction(
     if not callable(ai_callable):
         ai_callable = _default_ai_callable()
 
+    def new_decorator(
+        function_to_be_decorated: Callable[..., Any]
+    ) -> Callable[..., Any]:
+        _assert_function_has_return_type_annotation(function_to_be_decorated)
+        return_type_annotation = get_type_hints(function_to_be_decorated)["return"]
+
+        @wraps(function_to_be_decorated)
+        def wrapper(*args_inner: Any, **kwargs_inner: Any) -> Any:
+            prompt = prompt_function(
+                function_to_be_decorated, *args_inner, **kwargs_inner
+            )
+            ai_result = ai_callable(messages=prompt, **kwargs)  # type: ignore[misc]
+            return _parse_ai_result(
+                ai_result=ai_result, expected_return_type=return_type_annotation
+            )
+
+        return wrapper
+
+    return new_decorator(function) if function else new_decorator
+
     if function is not None:
         _assert_function_has_return_type_annotation(function)
         return_type_annotation = get_type_hints(function)["return"]
